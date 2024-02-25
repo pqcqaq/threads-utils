8efc727b0caf5d498170a83c7bcdd2391771805d
pqcqaq <905739777@qq.com>
2024-02-25 20:20:24 +0800

    修复了为null时会报错的bug

--- src/main/java/online/zust/qcqcqc/utils/threads/Promise.java
+++ src/main/java/online/zust/qcqcqc/utils/threads/Promise.java
@@ -1,7 +1,11 @@
 package online.zust.qcqcqc.utils.threads;
 
+import java.util.concurrent.Callable;
+
 /**
  * @author qcqcqc
  */
 public class Promise {
+    public Promise(Callable<?> callable) {
+    }
 }
